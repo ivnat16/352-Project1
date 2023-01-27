916c2598636adb84b5026c388bb6301d8951c6dd
ivnat16 <16tanvithigle@gmail.com>
2023-01-26 21:22:10 -0500

    correctly reversed and upped

--- client.py
+++ client.py
@@ -20,25 +20,28 @@ def client():
     server_binding = (localhost_addr, port)
     cs.connect(server_binding)
 
-    # send a intro message to the server.  
-    msg2 = "hello"
-    print("Data sent to server: ", msg2)
-    cs.send(msg2.encode('utf-8'))
+    # send a intro message to the server. 
+    with open('in-proj.txt') as f:
+        content = f.read() 
+        
+    # msg2 = "hello"
+    print("Data sent to server: ", content)
+    cs.send(content.encode('utf-8'))    
 
     # msg3 = "hello"
     # print("Data sent to client for uppercase: ", msg3)
     # cs.send(msg3.encode('utf-8'))
    
     # Receive data from the server
-    data_from_server=cs.recv(100)
+    data_from_server=cs.recv(200)
     print("[C]: Data received from server: {}".format(data_from_server.decode('utf-8')))
 
     # Receive data from the server
-    data_from_serverrev=cs.recv(100)
-    print("[C]: Reversed data received from server: {}".format(data_from_serverrev.decode('utf-8')))
+    data_from_serverrev=cs.recv(200)
+    print("[C]: Uppercase data received from server: {}".format(data_from_serverrev.decode('utf-8')))
 
-    data_from_serverup=cs.recv(100)
-    print("[C]: Uppercase data received from server: {}".format(data_from_serverup.decode('utf-8')))
+    data_from_serverup=cs.recv(200)
+    print("[C]: Reversed data received from server: {}".format(data_from_serverup.decode('utf-8')))
 
     # close the client socket
     cs.close()
--- server.py
+++ server.py
@@ -27,18 +27,24 @@ def server():
     csockid.send(msg.encode('utf-8'))
 
      # Receive data from the client
-    data_from_client=csockid.recv(100)
-    data = data_from_client.decode('utf-8')
-    #print("[C]: Data received from client: {}".format(data_from_client.decode('utf-8')))
-    datarev = data[::-1]
-    csockid.send(datarev.encode('utf-8'))
-
-    time.sleep(3)
-
+    data_from_client=csockid.recv(200)
     # data_from_client2=csockid.recv(100)
     data2 = data_from_client.decode('utf-8')
     dataup = data2.upper()
     csockid.send(dataup.encode('utf-8'))
+
+    data = data_from_client.decode('utf-8')
+    #print("[C]: Data received from client: {}".format(data_from_client.decode('utf-8')))
+    sentences = data.splitlines()
+    rev=''
+    for i in sentences:
+        rev = rev + i[::-1] +"\n"
+    #datarev = data[::-1]
+    csockid.send(rev.encode('utf-8'))
+
+    time.sleep(5)
+
+
 
 
     # Close the server socket
